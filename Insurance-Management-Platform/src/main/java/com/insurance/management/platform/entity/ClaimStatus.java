feb6cd6cfbb54e956c3098ca7da2a861699745e5
Amol Nandivale <nandivaleamol@gmail.com>
2023-04-05 09:25:45 +0530

    Initial commit

---
+++ Insurance-Management-Platform/src/main/java/com/insurance/management/platform/entity/ClaimStatus.java
@@ -0,0 +1,7 @@
+package com.insurance.management.platform.entity;
+
+public enum ClaimStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Insurance-Management-Platform-Backend
+ Insurance-Management-Platform-Backend REST API's Using Springboot
